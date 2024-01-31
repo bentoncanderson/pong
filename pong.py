3276730efd31ddae8ebcf3d585aece43f920107a
benton <bentonandersonwps@gmail.com>
2024-01-31 16:26:06 -0600

    work in progress

--- pong.py
+++ pong.py
@@ -9,11 +9,17 @@ window = pygame.display.set_mode((1200, 800))
 # fill screen white
 window.fill("black")
 
-# draw outlined rectangle
+# draw table
 pygame.draw.rect(window, "white", [50, 50, 1100, 700], 2)
 
-# draw line
+# draw line down middle
 pygame.draw.line(window, "white", [600, 50], [600, 750], 2)
+
+# draw player one's paddle
+pygame.draw.rect(window, "blue", [100, 100, 10, 100], 5)
+
+# draw player two's paddle 
+pygame.draw.rect(window, "blue", [1100, 100, 10, 100], 5)
 
 # update pygame
 while True: 
